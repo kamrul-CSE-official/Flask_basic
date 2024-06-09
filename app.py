0c3d00651d5fc4b65089288a7ff5b174b9d499f8
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-09 22:38:26 +0600

    Add templates

--- app.py
+++ app.py
@@ -1,16 +1,27 @@
-from flask import Flask 
+from flask import Flask, render_template  # type: ignore
+
 
 app = Flask(__name__)
+
+
+datas =  {
+    "name": "MD.Kamrul Hasan",
+    "numbers": [1,2,3,4,5,6,7,8,9,10]
+}
 
 @app.route("/home")
 @app.route("/")
 def home():
-    return "Bismillah"
+    return render_template('index.html', data=datas)
 
 @app.route("/about")
 def about():
-    return "This is about"
+    return render_template('about.html')
 
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
 
 if __name__ == "__main__": 
-    app.run()
+    app.run(debug=True)
