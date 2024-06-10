3ad1990462e9dd43aa8f8363165b1aff2b828d16
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-06-10 15:34:21 +0600

    add layout and also implemented update functions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect  # type: ignore
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -25,27 +25,45 @@ datas = [
     }
 ]
 
-@app.route("/home", methods = ["GET", "POST"])
+@app.route("/home", methods=["GET", "POST"])
 @app.route("/")
 def home():
     if request.method == "POST":
-        title = request.form["title"]
-        print("Title: ", title)
-        new_todo = {
-            "id": datas.__len__()+1,
-            "title": title
-        }
-        datas.append(new_todo)
-        
+        title = request.form.get("title")
+        if title:  # Check if title is not empty
+            new_id = max(todo["id"] for todo in datas) + 1 if datas else 1  # Ensure unique ID
+            new_todo = {
+                "id": new_id,
+                "title": title
+            }
+            datas.append(new_todo)
     return render_template('index.html', data=datas)
 
 @app.route("/remove/<int:todoid>")
 def removeTodo(todoid):
-    for todo in datas:
-        if todo["id"] == todoid:
-            datas.remove(todo)
+    todo_to_remove = next((todo for todo in datas if todo["id"] == todoid), None)
+    if todo_to_remove:
+        datas.remove(todo_to_remove)
     return redirect('/')
 
+
+@app.route("/update/<int:todoid>", methods=['GET'])
+def updateTodo(todoid):
+    for todo in datas:
+        if todo['id'] == todoid:
+            return render_template('update.html', todo=todo)
+    return redirect("/")
+
+
+@app.route("/updateTodo", methods=['POST'])
+def update():
+    title = request.form['title']
+    id = int(request.form['id'])  # Convert id to int for comparison
+    for todo in datas:
+        if todo['id'] == id:
+            todo['title'] = title
+            return redirect('/')
+    return redirect('/')
 
 @app.route("/about")
 def about():
